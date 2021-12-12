bdcc72db331391cd8d12e33521c4b6b365a34139
Awab Osman <awabosman3@gmail.com>
2021-12-12 05:30:20 -0500

    complete custom hooks

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,13 +2,29 @@ import { useState, useEffect } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  function transition(value) {
-    setMode(value)
+  function transition(value, replace = false) {
+    if (replace) {
+      let array = [...history]
+      array.pop();
+      setHistory(() => [...array, value]);
+      setMode(value)
+      return;
+    }
+    setMode(() => value);
+    setHistory((prev) => [...prev, value]);
   }
 
-  
-  
 
-  return { mode, transition };
+  function back() {
+    if (history.length > 1){
+      let array = [...history];
+      array.pop();
+      setHistory(() => array);
+      setMode(() => array[array.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
 }
