92a7f7667cb4b7b84713d9237f33632146ec2a4b
Awab Osman <awabosman3@gmail.com>
2021-12-16 17:23:53 -0500

    install and test cypress

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -20,7 +20,7 @@ export default function DayListItem(props) {
   }
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li data-testid="day" className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       {formatSpots()}
     </li>
